7aa51115b191ebceaa6353afb72dc01a00eae2a2
jeheecheon <jeheecheon@gmail.com>
2024-08-15 05:47:51 +0900

    feat: laptop 모델 및 블로그를 iframe으로 보여주도록 구현

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,28 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import Experience from "./Experience.tsx";
+import { Canvas } from "@react-three/fiber";
+import * as THREE from "three";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+import "./global.css";
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <Canvas
+      className="r3f"
+      dpr={[1, 2]}
+      gl={{
+        antialias: true,
+        outputColorSpace: THREE.SRGBColorSpace,
+      }}
+      camera={{
+        fov: 45,
+        near: 0.1,
+        far: 30,
+        position: [0, 0, 6],
+      }}
+    >
+      <Experience />
+    </Canvas>
+  </React.StrictMode>
+);
